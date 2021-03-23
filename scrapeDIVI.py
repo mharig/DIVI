30e7a247b8a51fc74e2555035da2a9277d60ecdf
Michael Harig <m.harig@gmx.de>
2021-03-23 18:46:28 -0400

    Update scrapeDIVI.py
    Removed external dependency.

--- scrapeDIVI.py
+++ scrapeDIVI.py
@@ -1,7 +1,8 @@
-import requests
+from urllib.request import urlopen
 import time
 t = time.localtime()
-filename = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_month}_{t.tm_day}-12-15.csv'
-data = requests.get(filename).text
+url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_month}_{t.tm_day}-12-15.csv'
+r = urlopen(url)
+data = r.read().decode()
 with open('DIVI.csv', 'ta') as f:
     f.write(data+'\n')
