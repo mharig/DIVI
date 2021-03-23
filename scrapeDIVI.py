ec08c10379e6525e66cbd7a522baebcbe29cdf09
Michael Harig <m.harig@gmx.de>
2021-03-23 19:08:38 -0400

    Update scrapeDIVI.py
    time struct names corrected.

--- scrapeDIVI.py
+++ scrapeDIVI.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import time
 t = time.localtime()
-url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_month}_{t.tm_day}-12-15.csv'
+url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_mon}_{t.tm_mday}-12-15.csv'
 r = urlopen(url)
 data = r.read().decode()
 with open('DIVI.csv', 'ta') as f:
