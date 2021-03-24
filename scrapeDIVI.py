610181720340c3e67d422e00529a10f29da1b2b2
Michael Harig <m.harig@gmx.de>
2021-03-24 08:46:57 -0400

    Update scrapeDIVI.py
    2digit day and month.

--- scrapeDIVI.py
+++ scrapeDIVI.py
@@ -1,7 +1,8 @@
 from urllib.request import urlopen
 from datetime import datetime, timedelta
 t = datetime.today() - timedelta(days=1)
-url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.year}-{t.month}-{t.day}_12-15.csv'
+url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.year}-{t.month:02d}-{t.day:02d}_12-15.csv'
+print(f'Fetching: {url}') 
 r = urlopen(url)
 data = r.read().decode()
 with open('DIVI.csv', 'ta') as f:
