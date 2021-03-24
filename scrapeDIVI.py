f9d40e235af4dc0b7e3edaae84d4fd7075956b05
Michael Harig <m.harig@gmx.de>
2021-03-24 08:39:46 -0400

    Update scrapeDIVI.py
    Changed URL date part to 1 day before today.

--- scrapeDIVI.py
+++ scrapeDIVI.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
-import time
-t = time.localtime()
-url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_mon}-{t.tm_mday}_12-15.csv'
+from datetime import datetime, timedelta
+t = datetime.today() - timedelta(days=1)
+url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.year}-{t.month}-{t.day}_12-15.csv'
 r = urlopen(url)
 data = r.read().decode()
 with open('DIVI.csv', 'ta') as f:
