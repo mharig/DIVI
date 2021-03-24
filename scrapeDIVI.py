f85b6d8a49863b55a4d6ec25b566b5b5ff73a1e5
Michael Harig <m.harig@gmx.de>
2021-03-24 08:22:27 -0400

    Update scrapeDIVI.py
    Typing error in URL corrected.

--- scrapeDIVI.py
+++ scrapeDIVI.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import time
 t = time.localtime()
-url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_mon}_{t.tm_mday}-12-15.csv'
+url = f'https://www.divi.de/joomlatools-files/docman-files/divi-intensivregister-tagesreports-csv/DIVI-Intensivregister_{t.tm_year}-{t.tm_mon}-{t.tm_mday}_12-15.csv'
 r = urlopen(url)
 data = r.read().decode()
 with open('DIVI.csv', 'ta') as f:
